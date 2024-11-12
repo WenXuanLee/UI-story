c4319b8ba2a79e35f4ab79579e63a36a3ff4b4d4
WenXuan Lee <wenxuanlee@WenXuandeMacBook-Air.local>
2024-11-12 14:20:43 +0000

    feat: init storybook and tailwind config and basic button story

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from "react"
+
+type ButtonProps = ComponentProps<"button">
+
+
+const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button className='text-blue-500' {...props} />
+  )
+}
+
+export default Button
